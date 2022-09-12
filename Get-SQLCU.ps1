fd758af6db37026fd12c880488d260702612ace3
ig212 <45108326+ig212@users.noreply.github.com>
2022-09-11 20:44:42 -0700

    Update Get-SQLCU.ps1

--- Get-SQLCU.ps1
+++ Get-SQLCU.ps1
@@ -1,100 +1,143 @@
 function Get-SqlCU{ [cmdletbinding()] param(
-  [Parameter(Mandatory)][Validateset(2016, 2017,2019)][int]$version
-)
-try{
-
-    if
-    (  $version -isnot [int]) {write-error "Integer not entered";write-output $Error[0]}
-    
-    else
-    {
+    [Parameter(Mandatory)][Validateset(2016, 2017,2019)][int]$version
+  )
+  try{
       if
-      ( $version -eq 2017)
+      (  $version -isnot [int]) {write-error "Integer not entered";write-output $Error[0]}
+      
+      else
       {
-        $sql_2017_dl="https://www.microsoft.com/en-us/download/confirmation.aspx?id=56128"
-        # this link will always be for the latest release of sql 2017 updates
-        $Invoke_CU= (Invoke-WebRequest -UseBasicParsing -Uri $sql_2017_dl -Method Get -Verbose).links.href|?{$_ -like "https://download.microsoft.com/download/C/4*"}| Select -First 1
-        $Retrieve_CU_KB_Name= $Invoke_CU.Substring(83,31)
-        $Folder_Name=$Retrieve_CU_KB_Name.Substring(14,9)
-        $path="C:\Sammy\sql2017\$Folder_Name"
-         if((Test-Path -Path $path) -eq $false)
-              { New-Item -ItemType Directory -Path $path -Verbose;
-                $Download_2017Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name -Verbose}
-         else{
-               if
-              (   ((get-childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name){write-host "KB exists in folder"}
-               else
-               {
+        if
+        ( $version -eq 2017)
+        {
+          $sql_2017_dl="https://www.microsoft.com/en-us/download/confirmation.aspx?id=56128"
+          # this link will always be for the latest release of sql 2017 updates
+          $Invoke_CU= (Invoke-WebRequest -UseBasicParsing -Uri $sql_2017_dl -Method Get -Verbose).links.href|?{$_ -like "https://download.microsoft.com/download/C/4*"}| Select -First 1
+          $Retrieve_CU_KB_Name= $Invoke_CU.Substring(83,31)
+          $Folder_Name=$Retrieve_CU_KB_Name.Substring(14,9)
+          $path="YourSharedPath\$Folder_Name"
+          ####### Smtp details for sending email to the team when an update is available
+          $SmtpServer="smtp.invalid.com"
+          $Subject_2017="SQL Cumulative Update $Folder_Name is Now available for SQL Server 2017 "
+          $Subject_2019="SQL Cumulative Update $Folder_Name is Now available for SQL Server 2019 "
+          $Body_2017="This is a Test. A new Cumulative update for SQL Server 2017 has been released and downloaded to \\uslcnas02\sqlsvrstage\SQL2017\service_pack\$Folder_Name "
+          $Body_2019="This is a Test. A new Cumulative update for SQL Server 2019 has been released and downloaded to \\uslcnas02\sqlsvrstage\SQL2019\service_pack\$Folder_Name "
+          $From="DoNotReply"
+          $To="Your email "
+          $Port=25
+           if((Test-Path -Path $path) -eq $false)
+                { New-Item -ItemType Directory -Path $path -Verbose;
                   $Download_2017Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name -Verbose
-               # Verify that the KB was successfully downloaded to the path 
-               if
-               (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name
-               )
-               {Write-Output "$Retrieve_CU_KB_Name was successfully downloaded to $path "
-               }
-               else
-               {
-                 Write-Output "$Retrieve_CU_KB_Name to $path was not successfully. Retry!"
-               }
-               }
-
-            }
-      }
-          
-      if
-      ( $version -eq 2019)
-      {
-        $sql_2019_dl="https://www.microsoft.com/en-us/download/confirmation.aspx?id=100809"
-        # this link will always be for the latest release of sql 2019 updates
-        $Invoke_CU= (Invoke-WebRequest -UseBasicParsing -Uri $sql_2019_dl -Method Get -Verbose).links.href|?{$_ -like "https://download.microsoft.com/download/6/e*"}| Select -First 1
-        $Retrieve_CU_KB_Name= $Invoke_CU.Substring(83,31)
-        $Folder_Name=$Retrieve_CU_KB_Name.Substring(14,9)
-        $path="C:\Sammy\sql2019\$Folder_Name"
-          if
-          ( (Test-Path -Path $path) -eq $false
-          )
-          { New-Item -ItemType Directory -Path $path -Verbose;
-            $Download_2019Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name\$Retrieve_CU_KB_Name -Verbose
-          }
-          else
-          {
-            if
-              (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name){write-host "KB exists in folder"}
-              else
-              {
-                 $Download_2019Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name -Verbose
-              }
-              # Verify that the KB was successfully downloaded to the path 
-              if
-              (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name
-              )
-              {Write-Output "$Retrieve_CU_KB_Name was successfully downloaded to $path "
-              }
-              else
-              {
-                Write-Output "$Retrieve_CU_KB_Name to $path was not successfully. Retry!"
-              }
-          }
-      }
-      elseif
-      (
-        $version -eq  2016
-      )
-      {
-         Write-Output "sql $version is currently unsupported. "
-      }
-
+                     # Verify that the KB was successfully downloaded to the path 
+                       if
+                       (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name
+                       )
+                       {Write-Output "$Retrieve_CU_KB_Name was successfully downloaded to $path "
+                       Send-MailMessage -SmtpServer $SmtpServer -Body $Body_2017 -From $From -To $To -Subject $Subject_2017 -Priority High -Port $Port -BodyAsHtml -verbose                
+                       }
+                       else
+                       {
+                         Write-Output " Unable to download $Retrieve_CU_KB_Name to $path. Retry!"
+                       }
+                }
+           else{
+                 if
+                (   ((get-childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name){Write-Output "KB exists in folder"}
+                 else
+                 {
+                    $Download_2017Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name -Verbose
+                 # Verify that the KB was successfully downloaded to the path 
+                    if
+                    (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name
+                    )
+                    {Write-Output "$Retrieve_CU_KB_Name was successfully downloaded to $path "
+                    Send-MailMessage -SmtpServer $SmtpServer -Body $Body_2017 -From $From -To $To -Subject $Subject_2017 -Priority High -Port $Port -BodyAsHtml -verbose                
+                    }
+                    else
+                    {
+                      Write-Output "$Retrieve_CU_KB_Name to $path was not successfully. Retry!"
+                    }
+                 }
   
-
-    } 
-
-
-}
-
-
-catch
-{
-    Write-Warning $Error[0]
-}
-
-}
+              }
+        }
+            
+        if
+        ( $version -eq 2019)
+        {
+          $sql_2019_dl="https://www.microsoft.com/en-us/download/confirmation.aspx?id=100809"
+          # this link will always be for the latest release of sql 2019 updates
+          $Invoke_CU= (Invoke-WebRequest -UseBasicParsing -Uri $sql_2019_dl -Method Get -Verbose).links.href|?{$_ -like "https://download.microsoft.com/download/6/e*"}| Select -First 1
+          $Retrieve_CU_KB_Name= $Invoke_CU.Substring(83,31)
+          $Folder_Name=$Retrieve_CU_KB_Name.Substring(14,9)
+          $path="\\YourSharedPath\$Folder_Name"
+          ####### Smtp details for sending email to the team when an update is available
+          $SmtpServer="smtp.invalid.com"
+          $Subject_2017="SQL Cumulative Update $Folder_Name is Now available for SQL Server 2017 "
+          $Subject_2019="SQL Cumulative Update $Folder_Name is Now available for SQL Server 2019 "
+          $Body_2017="This is a Test. A new Cumulative update for SQL Server 2017 has been released and downloaded to \\YourSharedPath\$Folder_Name "
+          $Body_2019="This is a Test. A new Cumulative update for SQL Server 2019 has been released and downloaded to \\YourSharedPath\$Folder_Name "
+          $From="DoNotReply"
+          $To="Your email "
+          $Port=25
+            if
+            ( (Test-Path -Path $path) -eq $false
+            )
+            { New-Item -ItemType Directory -Path $path -Verbose;
+              $Download_2019Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name\ -Verbose
+                   # Verify that the KB was successfully downloaded to the path 
+                   if
+                   (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name
+                   )
+                   {Write-Output "$Retrieve_CU_KB_Name was successfully downloaded to $path "
+                   Send-MailMessage -SmtpServer $SmtpServer -Body $Body_2019 -From $From -To $To -Subject $Subject_2019 -Priority High -Port $Port -Body
+                   }
+                   else
+                   {
+                     Write-Output " Unable to download $Retrieve_CU_KB_Name to $path. Retry!"
+                   }
+              ####
+            }
+            else
+            {
+              if
+                (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name){write-host "KB exists in folder"}
+                else
+                {
+                   $Download_2019Update= Invoke-WebRequest -UseBasicParsing -Uri $Invoke_CU -Method Get -OutFile $path\$Retrieve_CU_KB_Name -Verbose
+                # Verify that the KB was successfully downloaded to the path 
+                    if
+                    (   ((Get-Childitem -Path $path).Name) -eq $Retrieve_CU_KB_Name
+                    ) 
+                    {Write-Output "$Retrieve_CU_KB_Name was successfully downloaded to $path "
+                    Send-MailMessage -SmtpServer $SmtpServer -Body $Body_2019 -From $From -To $To -Subject $Subject_2019 -Priority High -Port $Port -BodyAsHtml -verbose
+                    }
+                    else
+                    {
+                      Write-Output "Unable to download $Retrieve_CU_KB_Name to $path. Retry!"
+                    }
+                }
+            }
+        }
+        elseif
+        (
+          $version -eq  2016
+        )
+        {
+           Write-Output "sql $version is currently unsupported. "
+        }
+  
+    
+  
+      } 
+  
+  
+  }
+  
+  
+  catch
+  {
+      Write-Warning $Error[0]
+  }
+  
+  }
